13f50f6fda3f5f00440e58e9a3d8cea869ab6ab4
Thib Guicherd-Callin <thib@cs.stanford.edu>
2017-07-11 16:00:10 -0700

    Add /bin/bash for $'x' construct.

--- demo.d/3.extract-metadata.sh
+++ demo.d/3.extract-metadata.sh
@@ -1,3 +1,4 @@
+#/bin/bash
 while IFS=$'\t' read -r auid filename ; do
   json="{\"auid\":\"$auid\",\"updateType\":\"full_extraction\"}"
   curl -X POST -H "Content-Type:application/json" -d $json http://localhost:8083/mdupdates
