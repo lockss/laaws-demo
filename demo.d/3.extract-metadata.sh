25f7224e588e2b8de1bac70b684ff506ea13753a
fergaloy <fergaloy@stanford.edu>
2017-07-12 14:48:22 -1000

    Changed how to locate the AU map file.

--- demo.d/3.extract-metadata.sh
+++ demo.d/3.extract-metadata.sh
@@ -1,5 +1,8 @@
 #/bin/bash
+WARC_SRC=`pwd`/warcs
+AUID_MAP=$WARC_SRC/auidmap.txt
+
 while IFS=$'\t' read -r auid filename ; do
   json="{\"auid\":\"$auid\",\"updateType\":\"full_extraction\"}"
   curl -X POST -H "Content-Type:application/json" -d $json http://localhost:8083/mdupdates
-done < "../warcs/auidmap.txt"
+done < $AUID_MAP
