ac985b758346bdf5931f189680fbf24ba2586b07
Daniel Vargas <dlvargas@stanford.edu>
2017-07-13 09:37:10 -0700

    Lighter approach: Clean up only LAAWS related containers and images

--- clean-docker.sh
+++ clean-docker.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-docker ps -a -q | xargs docker stop
-docker ps -a -q | xargs docker rm
-docker images -q | xargs docker rmi --force
+
+# Stop Docker Compose in case its running
+docker-compose stop
+
+# Ensure LAAWS containers are stopped and remove them
+docker ps -a | grep laaws | awk '{print $1}' | xargs docker stop
+docker ps -a | grep laaws | awk '{print $1}' | xargs docker rm
+
+# Remove LAAWS images
+docker images | grep laaws | awk '{print $3}' | xargs docker rmi --force
