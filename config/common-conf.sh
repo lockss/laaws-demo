0dcbe66126c2b63bad9968b9642d47c9f3be9a26
Thib Guicherd-Callin <thib@cs.stanford.edu>
2018-04-27 02:27:05 -0700

    Squashed commit of the following:
    commit f28b5a94d418dfe9a5fa488607774936e6cd8560
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Fri Apr 27 02:22:27 2018 -0700
    
        Variant-dependent repo host. Parameterize repo command.
    
    commit bce9b0671e49dce0212aac27a820cda5557493fa
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Fri Apr 27 00:50:05 2018 -0700
    
        Variant-dependent config files (currently only needed for the host of the Postgres database).
    
    commit dac46102d16204b9c954e8a1201737abc1b3980b
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Fri Apr 27 00:22:37 2018 -0700
    
        Parameterize Postgres port and add standalone Postgres Docker container convenience command.
    
    commit 8c6a7fed4e861908728fe8f2d7be8354b87e2f97
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Thu Apr 26 23:11:32 2018 -0700
    
        Beginning of the plain rig
    
    commit 3274b0267652807c35567d21fe36336819ed4e82
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Thu Apr 26 22:36:30 2018 -0700
    
        Refactor Docker Compose rig and plain shell rig into common parts.
        (Shell needs more work, Docker Compose works.)

---
+++ config/common-conf.sh
@@ -0,0 +1,69 @@
+# Copyright (c) 2000-2018, Board of Trustees of Leland Stanford Jr. University
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+MVN_GRP=org.lockss.laaws
+
+CFG_GRP="${MVN_GRP}"
+CFG_ART='laaws-configuration-service'
+CFG_VERSION='1.1.0-SNAPSHOT'
+CFG_PORT='54420'
+CFG_UI='54421'
+CFG_CMD="-l config/lockss-configuration-service/common.xml -l config/laaws-demo.txt -l config/laaws-demo.opt -l config/laaws-demo.${VARIANT}.txt -l config/laaws-demo.${VARIANT}.opt -p config/lockss-configuration-service/lockss.txt -p config/lockss-configuration-service/lockss.opt"
+
+MDQ_GRP="${MVN_GRP}"
+MDQ_ART='laaws-metadata-service'
+MDQ_VERSION='1.0.0-SNAPSHOT'
+MDQ_PORT='49520'
+MDQ_UI='49521'
+MDQ_CMD="-c http://lockss-u:lockss-p@${CFG_HOST}:${CFG_PORT} -p http://${CFG_HOST}:${CFG_PORT}/config/file/cluster -p config/lockss-metadata-service/lockss.txt -p config/lockss-metadata-service/lockss.opt -p config/lockss-metadata-service/lockss.${VARIANT}.txt -p config/lockss-metadata-service/lockss.${VARIANT}.opt"
+
+MDX_GRP="${MVN_GRP}"
+MDX_ART='laaws-metadata-extraction-service'
+MDX_VERSION='1.1.0-SNAPSHOT'
+MDX_PORT='28120'
+MDX_UI='28121'
+MDX_CMD="-c http://lockss-u:lockss-p@${CFG_HOST}:${CFG_PORT} -p http://${CFG_HOST}:${CFG_PORT}/config/file/cluster -p config/lockss-metadata-extraction-service/lockss.txt -p config/lockss-metadata-extraction-service/lockss.opt -p config/lockss-metadata-extraction-service/lockss.${VARIANT}.txt -p config/lockss-metadata-extraction-service/lockss.${VARIANT}.opt"
+
+POL_GRP="${MVN_GRP}"
+POL_ART='laaws-poller'
+POL_VERSION='1.0.0-SNAPSHOT'
+POL_PORT='25250'
+POL_UI='25251'
+POL_CMD="-c http://lockss-u:lockss-p@${CFG_HOST}:${CFG_PORT} -p http://${CFG_HOST}:${CFG_PORT}/config/file/cluster -p config/lockss-poller/lockss.txt -p config/lockss-poller/lockss.opt"
+
+REPO_GRP="${MVN_GRP}"
+REPO_ART='laaws-repository-service'
+REPO_VERSION='1.8.0-SNAPSHOT'
+REPO_PORT='32640'
+REPO_CMD="-Drepo.spec=custom -Drepo.index.spec=solr -Drepo.index.solr.solrUrl=XXX -Drepo.datastore.spec=hdfs -Drepo.datastore.hdfs.basedir=/lockss -Drepo.datastore.hdfs.server=YYY"
+
+PGSQL_VERSION='9.6'
+PGSQL_PORT='5432'
+POSTGRES_PASSWORD=goodPassword
+POSTGRES_USER=LOCKSS
+POSTGRES_DB=postgres
